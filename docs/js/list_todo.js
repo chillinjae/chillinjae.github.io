14f145c939052f849359aa45dfa070b0d37c961e
Jaeyoung Choi <jae@chillin.dev>
2021-01-31 17:56:47 -0800

    update posts

--- docs/js/list_todo.js
+++
@@ -1,5 +0,0 @@
-var inputs = document.querySelectorAll("li p input")
-for (let i = 0; i <inputs.length; i++){
-    inputs[i].parentElement.parentElement.classList.add('task-list')
-    inputs[i].parentElement.parentElement.parentElement.classList.add('task-list-container')
-}
